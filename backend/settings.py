61f7f60a10d62064960192b40a09d68a2012f2e4
saqsham <saqsham@gmail.com>
2020-04-15 10:53:18 +0530

    inti4

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')zg6io98c%%9#*-s2#2mz1(j-#+z5+^(1uwqz$^dqib%!orl3j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
