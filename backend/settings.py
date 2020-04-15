38feba0214ff932b3f727b9c33db08a721354543
saqsham <saqsham@gmail.com>
2020-04-15 10:58:15 +0530

    init5

--- backend/settings.py
+++ backend/settings.py
@@ -59,7 +59,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, '../frontend/templates'),
+            os.path.join(BASE_DIR, 'frontend/templates'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
